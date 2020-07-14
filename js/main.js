8addf8517119a31c74857baef0a010f8fa0b64dd
sazzat <sazzat1000@gmail.com>
2020-07-14 10:23:57 +0600

    Slider test 1

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
 $(document).ready(function () {
   // Sticky Nav
   
-  var menu_top_offset = $("nav").offset().top;
+  // var menu_top_offset = $("nav").offset().top;
 
-  $(window).on("scroll", function () {
-    var scrollSize = $(window).scrollTop();
+  // $(window).on("scroll", function () {
+  //   var scrollSize = $(window).scrollTop();
 
-    if (scrollSize > menu_top_offset) {
-      $("nav").addClass("sticky-nav");
-    } else {
-      $("nav").removeClass("sticky-nav");
-    }
-  });
+  //   if (scrollSize > menu_top_offset) {
+  //     $("nav").addClass("sticky-nav");
+  //   } else {
+  //     $("nav").removeClass("sticky-nav");
+  //   }
+  // });
 
   // Back to Top Button
 
   $(".footer-icon").click(function () {
     $("html, body").animate({ scrollTop: 0 }, 1000);
   });
+
+  // Service Slider
+  $(".service-slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 800,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    draggable: false
+  })
 });
