2a5d753de343ae4ff37bf22903d55e1e849eddfc
sazzat <sazzat1000@gmail.com>
2020-07-14 17:36:08 +0600

    All Slider added

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
   // Sticky Nav
   
-  // var menu_top_offset = $("nav").offset().top;
+  var menu_top_offset = $("nav").offset().top;
 
-  // $(window).on("scroll", function () {
-  //   var scrollSize = $(window).scrollTop();
+  $(window).on("scroll", function () {
+    var scrollSize = $(window).scrollTop();
 
-  //   if (scrollSize > menu_top_offset) {
-  //     $("nav").addClass("sticky-nav");
-  //   } else {
-  //     $("nav").removeClass("sticky-nav");
-  //   }
-  // });
+    if (scrollSize > menu_top_offset) {
+      $("nav").addClass("sticky-nav");
+    } else {
+      $("nav").removeClass("sticky-nav");
+    }
+  });
 
   // Back to Top Button
 
@@ -29,8 +29,18 @@ $(document).ready(function () {
     draggable: false,
     prevArrow: "<i class='fas fa-long-arrow-alt-left service-prev'></i>",
     nextArrow: "<i class='fas fa-long-arrow-alt-right service-next'></i>"
-  })
+  });
 
+  // Portfolio Slider 
+  $(".portfolio-slider").slick({
+    speed: 800,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    draggable: false,
+    prevArrow: "<i class='fas fa-angle-left portfolio-prev'></i>",
+    nextArrow: "<i class='fas fa-angle-right portfolio-next'></i>",
+  });
+  
   // Testimonial Slider 
   $(".testimonial-slider").slick({
     autoplay: true,
@@ -39,7 +49,18 @@ $(document).ready(function () {
     slidesToShow: 2,
     slidesToScroll: 2,
     draggable: false,
-    // prevArrow: $(".testimonial-prev"),
-    // nextArrow: $(".testimonial-next")
-  })
+    prevArrow: "<i class='fas fa-angle-left testimonial-prev'></i>",
+    nextArrow: "<i class='fas fa-angle-right testimonial-next'></i>",
+  });
+
+  // Blog Slider 
+  $(".blog-slider").slick({
+    speed: 800,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    draggable: false,
+    centerPadding: '0',
+    prevArrow: "<i class='fas fa-angle-left blog-prev'></i>",
+    nextArrow: "<i class='fas fa-angle-right blog-next'></i>",
+  });
 });
